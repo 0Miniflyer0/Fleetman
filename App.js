d6a69703aa8c2ce3b768bbb6ab8aa644c17db04a
0Miniflyer0 <145483845+0Miniflyer0@users.noreply.github.com>
2025-06-16 11:07:45 -0600

    Navigation Frame

--- App.js
+++ App.js
@@ -1,27 +1,20 @@
-// App.js
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import HomeScreen from 'Fleetman/screens/HomeScreen.js';
+import SettingsScreen from 'Fleetman/screens/SettingsScreen.js';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>🚀 Hello from Expo!</Text>
-      <Button title="Press me" onPress={() => Alert.alert('Button pressed!')} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-});
